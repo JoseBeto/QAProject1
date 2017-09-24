16e6dc47a667e6f6ec98042518dac3ebdd390826
Jose Bocanegra <JBocanegra96@gmail.com>
2017-09-24 12:47:15 -0500

    First commit.

---
+++ commons-email-1.3.2-src/test/main/java/org/apache/commons/mail/EmailTest.java
@@ -0,0 +1,66 @@
+package org.apache.commons.mail;
+import org.junit.Test;
+import junit.framework.TestCase;
+
+public class EmailTest extends TestCase {
+	
+	@Test
+    public void testAddBcc() throws Exception {
+		//assertEquals, expected value first
+    }
+	
+	@Test
+    public void testAddCc() throws Exception {
+        
+    }
+	
+	@Test
+    public void testAddHeader() throws Exception {
+        
+    }
+	
+	@Test
+    public void testAddReplyTo() throws Exception {
+        
+    }
+	
+	@Test
+    public void testBuildMimeMessage() throws Exception {
+        
+    }
+	
+	@Test
+    public void testGetHostName() throws Exception {
+        
+    }
+	
+	@Test
+    public void testGetMailSession() throws Exception {
+        
+    }
+	
+	@Test
+    public void testGetSentDate() throws Exception {
+        
+    }
+	
+	@Test
+    public void testGetSocketConnectionTimeout() throws Exception {
+        
+    }
+	
+	@Test
+    public void testSend() throws Exception {
+        
+    }
+	
+	@Test
+    public void testSetFrom() throws Exception {
+        
+    }
+	
+	@Test
+    public void testUpdateContentType() throws Exception {
+        
+    }
+}
